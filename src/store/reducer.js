2565af36d9e82caad15eda917adaa7bbc1377fb8
wenzaidihu <750625920@qq.com>
2019-12-26 23:47:05 +0800

    给src/commom/header/store下添加入口文件index.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import headerReducer from '../common/header/store/reducer';
+import { reducer as headerReducer} from '../common/header/store';
 
 const reducer = combineReducers ({
 	header: headerReducer
