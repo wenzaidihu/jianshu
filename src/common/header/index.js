a0c70d23d57c51320fdf128b51d2e928155617ae
wenzaidihu <750625920@qq.com>
2019-12-27 22:38:06 +0800

    避免无意义的请求发送,提升组件性能

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -58,7 +58,7 @@ class Header extends Component {
   }
 
   render() {
-    const { focused, handleInputFocus, handleInputBlur } = this.props;
+    const { focused, handleInputFocus, handleInputBlur, list } = this.props;
     return (
       <HeaderWrapper>
         <Logo />
@@ -78,7 +78,7 @@ class Header extends Component {
             >
               <NavSearch
                 className={focused ? 'focused': ''}
-                onFocus={handleInputFocus}
+                onFocus={() => handleInputFocus(list)}
                 onBlur={handleInputBlur}
               ></NavSearch>
             </CSSTransition>
@@ -111,8 +111,8 @@ const mapStateToProps = (state) => {
 
 const mapDispathToProps = (dispatch) => {
   return {
-    handleInputFocus() {
-      dispatch(actionCreators.getList());
+    handleInputFocus(list) {
+      (list.size === 0) && dispatch(actionCreators.getList());
       dispatch(actionCreators.searchFocus());
     },
     handleInputBlur() {
