e6945624b1352a1399292431a6d06b588983f877
wenzaidihu <750625920@qq.com>
2019-12-27 16:23:06 +0800

    把header恢复成普通的组件

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -20,68 +20,73 @@ import {
 } from './style';
 import { bindActionCreators } from 'redux';
 
-const getListArea = (show) => {
-  if (show) {
+class Header extends Component {
+
+  getListArea (show) {
+    if (show) {
+      return (
+        <SearchInfo>
+          <SearchInfoTitle>
+            热门搜索
+            <SearchInfoSwitch>
+              换一批
+            </SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+          </SearchInfoList>
+        </SearchInfo>
+      )
+    }else {
+      return null;
+    }
+  }
+  
+  render() {
     return (
-      <SearchInfo>
-            <SearchInfoTitle>
-              热门搜索
-              <SearchInfoSwitch>
-                换一批
-              </SearchInfoSwitch>
-            </SearchInfoTitle>
-            <SearchInfoList>
-              <SearchInfoItem>教育</SearchInfoItem>
-              <SearchInfoItem>教育</SearchInfoItem>
-              <SearchInfoItem>教育</SearchInfoItem>
-              <SearchInfoItem>教育</SearchInfoItem>
-              <SearchInfoItem>教育</SearchInfoItem>
-              <SearchInfoItem>教育</SearchInfoItem>
-            </SearchInfoList>
-          </SearchInfo>
-    )
-  }else {
-    return null;
+      <HeaderWrapper>
+        <Logo />
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载App</NavItem>
+          <NavItem className="right">登录</NavItem>
+          <NavVip></NavVip>
+          <NavItem className="right">
+            <span className="iconfont">&#xe636;</span>
+          </NavItem>
+          <SearWrapper>
+            <CSSTransition
+              in={this.props.focused}
+              timeout={200}
+              classNames="slide"
+            >
+              <NavSearch
+                className={this.props.focused ? 'focused': ''}
+                onFocus={this.props.handleInputFocus}
+                onBlur={this.props.handleInputBlur}
+              ></NavSearch>
+            </CSSTransition>
+            <span className={this.props.focused ? 'focused iconfont': 'iconfont'}>&#xe6e4;</span>
+            {this.getListArea(this.props.focused)}
+          </SearWrapper>
+        </Nav>
+        <AddItion>
+          <Button className="writting">
+            <span className="iconfont">&#xe615;</span>
+            写文章
+          </Button>
+          <Button className="reg">注册</Button>
+        </AddItion>
+      </HeaderWrapper>
+    );
   }
 }
-const Header = (props) => {
-  return(
-    <HeaderWrapper>
-      <Logo />
-      <Nav>
-        <NavItem className="left active">首页</NavItem>
-        <NavItem className="left">下载App</NavItem>
-        <NavItem className="right">登录</NavItem>
-        <NavVip></NavVip>
-        <NavItem className="right">
-          <span className="iconfont">&#xe636;</span>
-        </NavItem>
-        <SearWrapper>
-          <CSSTransition
-            in={props.focused}
-            timeout={200}
-            classNames="slide"
-          >
-            <NavSearch
-              className={props.focused ? 'focused': ''}
-              onFocus={props.handleInputFocus}
-              onBlur={props.handleInputBlur}
-            ></NavSearch>
-          </CSSTransition>
-          <span className={props.focused ? 'focused iconfont': 'iconfont'}>&#xe6e4;</span>
-          {getListArea(props.focused)}
-        </SearWrapper>
-      </Nav>
-      <AddItion>
-        <Button className="writting">
-          <span className="iconfont">&#xe615;</span>
-          写文章
-        </Button>
-        <Button className="reg">注册</Button>
-      </AddItion>
-    </HeaderWrapper>
-  );
-}
+
 
 const mapStateToProps = (state) => {
   return {
