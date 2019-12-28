d8e7d97cf14e7af9be0011a448fd14298481292c
wenzaidihu <750625920@qq.com>
2019-12-28 18:23:07 +0800

    首页组件的拆分

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
-import { HomeWrapper } from './style';
+import Topic from './components/Topic';
+import List from './components/List';
+import Recommend from './components/Recommend';
+import Writer from './components/Writer';
+import {
+  HomeWrapper,
+  HomeLeft,
+  HomeRight,
+} from './style';
 
 class Home extends Component {
   render() {
     return (
-      <div>home</div>
+      <HomeWrapper>
+        <HomeLeft>
+          <img className="banner-img" src="https://upload.jianshu.io/admin_banners/web_images/4852/8e9c711020897ddd2964898b6608348635be154c.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"></img>
+          <Topic />
+          <List />
+        </HomeLeft>
+        <HomeRight>
+          <Recommend />
+          <Writer />
+        </HomeRight>
+      </HomeWrapper>
     )
   }
 }
