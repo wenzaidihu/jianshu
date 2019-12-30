409efc292e2dc865bf41d8bc107778456dffc6c2
wenzaidihu <750625920@qq.com>
2019-12-30 22:17:36 +0800

    更多文字布局

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ListItem, ListInfo } from '../style';
+import { ListItem, ListInfo, LoadMore } from '../style';
 import { connect } from 'react-redux';
 
 class List extends Component {
@@ -13,13 +13,14 @@ class List extends Component {
 						<ListItem key={item.get('id')}>
 							<img alt='' className='pic' src={item.get('imgUrl')} />
 							<ListInfo>
-					<h3 className='title'>{item.get('title')}</h3>
-					<p className='desc'>{item.get('desc')}</p>
+							<h3 className='title'>{item.get('title')}</h3>
+							<p className='desc'>{item.get('desc')}</p>
 							</ListInfo>
 						</ListItem>
 						);
 					})
 				}
+				<LoadMore>更多文字</LoadMore>
 			</div>
 		)
 	}
