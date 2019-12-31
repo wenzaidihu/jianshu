3c406d7d48aa0f5aebe8c15562eda3d3bf0c53f8
wenzaidihu <750625920@qq.com>
2019-12-31 16:37:37 +0800

    返回顶部功能实现

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,7 +4,8 @@ import Topic from './components/Topic';
 import List from './components/List';
 import Recommend from './components/Recommend';
 import Writer from './components/Writer';
-import { actionCreators } from './store'
+import { actionCreators } from './store';
+import { BackTop } from './style';
 import {
   HomeWrapper,
   HomeLeft,
@@ -12,6 +13,11 @@ import {
 } from './style';
 
 class Home extends Component {
+  
+  handleScrollTop() {
+		window.scrollTo(0, 0);
+	}
+
   render() {
     return (
       <HomeWrapper>
@@ -24,19 +30,40 @@ class Home extends Component {
           <Recommend />
           <Writer />
         </HomeRight>
+        { this.props.showScroll ? <BackTop onClick={this.handleScrollTop}>顶部</BackTop> : null}
       </HomeWrapper>
     )
   }
+
   componentDidMount() {
     this.props.changeHomeData();
+    this.bindEvents();
+  }
+
+  componentWillUnmount() {
+		window.removeEventListener('scroll', this.props.changeScrollTopShow);
+	}
+
+  bindEvents() {
+    window.addEventListener('scroll', this.props.changeScrollTopShow);
   }
 }
 
+const mapState = (state) => ({
+	showScroll: state.getIn(['home', 'showScroll'])
+})
+
 const mapDispatch = (dispatch) => ({
 	changeHomeData() {
-    const action = actionCreators.getHomeInfo();
-    dispatch(action);
+    dispatch(actionCreators.getHomeInfo());
+  },
+  changeScrollTopShow(e) {
+		if (document.documentElement.scrollTop > 100) {
+			dispatch(actionCreators.toggleTopShow(true))
+		}else {
+			dispatch(actionCreators.toggleTopShow(false))
+    }
 	}
 });
 
-export default connect(null, mapDispatch)(Home);
+export default connect(mapState, mapDispatch)(Home);
