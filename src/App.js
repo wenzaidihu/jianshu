5aaf17ff9bc2e68b50625107b7e550209059c20c
wenzaidihu <750625920@qq.com>
2019-12-31 18:27:09 +0800

    详情页面布局

--- src/App.js
+++ src/App.js
@@ -5,22 +5,20 @@ import { GlobalStyleFont } from './statics/iconfont/iconfont'
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './common/header/index';
 import Home from './pages/home';
-import Detail from './pages/detail';
+import Detail from './pages/detail/index';
 import store from './store';
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <GlobalStyle />
       <GlobalStyleFont />
-      <Provider store={store}>
+      <BrowserRouter>
         <Header />
-        <BrowserRouter>
-          <Route path='/' exact component={Home}></Route>
-          <Route path='/detail' exact component={Detail}></Route>
-        </BrowserRouter>
-      </Provider>
-    </div>
+        <Route path='/' exact component={Home}></Route>
+        <Route path='/detail' exact component={Detail}></Route>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import { DetailWrapper, Header, Content } from './style';
 
-class Detail extends Component {
-  render() {
-    return (
-      <div>detail</div>
-    )
-  }
+class Detail extends PureComponent {
+	render() {
+		return (
+			<DetailWrapper>
+				<Header>一个新开始</Header>
+        <Content>
+				  <img src='//upload-images.jianshu.io/upload_images/14359384-192535e30279e007.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'></img>
+          <p>昨天，我的许多文章被封，心里不舒服，发文吐槽。发泄也发泄过了，作为写手，本着对自己负责，对读者负责，我一篇篇检查以前的文章，发现不满意的居多。以前也发现了，想删除，想私密，虚荣心作怪，没下去手。
+
+这些曾经的练笔，就像自己的孩子，虽然有着这样那样的缺点，我的汗水流淌在这里，多少个日日夜夜，我把心用在这里。自己辛苦码出的字，说没有感情是假的，怎么舍得删除呢？
+
+如今简书有了动作，我也借这个机会，痛下杀手，私密一些文章，让连载质量更高一点。
+
+祸兮福所倚，简书的动作让我有了修改《夭言不惑众》这本连载的想法。修改文章，对我是一次提高。修改后的文章，也会让读文的朋友时间花得更值一点。
+
+当然，修改后的文集，篇幅和字数都会减少。写作小白的我，更文字数的增加也是对自己的鼓励，不舍啊。今年1月1号，我定下更新原创文字三十万的计划，目前已完成十二万加。那就从现在开始，更新原创文字二十万。说到计划，我还有今年读十本书的阅读计划。数量上来说，算是超额完成，因为截止到今天，已读完76本。读书笔记应该有几千或者上万条吧?拆书计划估计在简书是行不通了。以后的文字方向是散文、随笔、小说，读书笔记就私密不发了。说到这里，还有个怪现象，在简书阅读量高的文章，在公众号上就几十个阅读，过百都难，这个正常，我的公众号小。可在这里阅读量几百的文章，到公众号上阅读过千，我就糊涂了，搭档也弄不明白。
+
+好风凭借力，简书最后会走向何方，我不知道。暂时还没想离开，就好好更文，继续精进自己。
+
+</p>
+        </Content>
+      </DetailWrapper>
+		)
+	}
 }
 
 export default Detail;
