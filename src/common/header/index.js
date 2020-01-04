2e03cfb95b40778f47831784d774e8c2c918260c
wenzaidihu <750625920@qq.com>
2020-01-04 18:26:09 +0800

    登录功能实现

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -3,6 +3,7 @@ import { CSSTransition } from 'react-transition-group';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { actionCreators } from './store';
+import { actionCreators as loginActionCreators } from '../../pages/login/store'
 import {
   HeaderWrapper,
   Logo,
@@ -59,7 +60,7 @@ class Header extends Component {
   }
 
   render() {
-    const { focused, handleInputFocus, handleInputBlur, list } = this.props;
+    const { focused, handleInputFocus, handleInputBlur, list, login, logout } = this.props;
     return (
       <HeaderWrapper>
         <Link to='/'>
@@ -68,7 +69,11 @@ class Header extends Component {
         <Nav>
           <NavItem className="left active">首页</NavItem>
           <NavItem className="left">下载App</NavItem>
-          <NavItem className="right">登录</NavItem>
+          {
+						login ? 
+							<NavItem className='right' onClick={logout}>退出</NavItem> : 
+							<Link to='/login'><NavItem className='right'>登录</NavItem></Link>
+					}
           <NavVip></NavVip>
           <NavItem className="right">
             <span className="iconfont">&#xe636;</span>
@@ -108,7 +113,8 @@ const mapStateToProps = (state) => {
     list: state.getIn(['header','list']),
     page: state.getIn(['header','page']),
     totalPage: state.getIn(['header','totalPage']),
-    mouseIn: state.getIn(['header','mouseIn'])
+    mouseIn: state.getIn(['header','mouseIn']),
+    login: state.getIn(['login','login'])
   }
 }
 
@@ -141,7 +147,10 @@ const mapDispathToProps = (dispatch) => {
       }else {
         dispatch(actionCreators.changePage(1));
       }
-    }
+    },
+    logout() {
+			dispatch(loginActionCreators.logout())
+		}
   }
 }
 
