4d00078821a41c0dda805356117133fdc7b9d3eb
wenzaidihu <750625920@qq.com>
2020-01-05 23:30:04 +0800

    change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { GlobalStyle } from './style';
 import { GlobalStyleFont } from './statics/iconfont/iconfont'
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Header from './common/header/index';
 import Home from './pages/home';
 import Detail from './pages/detail/loadable';
@@ -10,18 +10,20 @@ import Login from './pages/login';
 import Write from './pages/write';
 import store from './store';
 
+// BrowserRouter: http://localhost:3000/login
+// HashRouter: http://localhost:3000/#/login
 function App() {
   return (
     <Provider store={store}>
       <GlobalStyle />
       <GlobalStyleFont />
-      <BrowserRouter>
+      <HashRouter>
         <Header />
         <Route path='/' exact component={Home}></Route>
         <Route path='/login' exact component={Login}></Route>
         <Route path='/write' exact component={Write}></Route>
         <Route path='/detail/:id' exact component={Detail}></Route>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   );
 }
